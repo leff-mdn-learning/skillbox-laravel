f1b5b359b0870ce985877a79fc4ad7e8ffd1bf4d
leffo <leffalt@gmail.com>
2020-12-17 12:07:43 +0700

    add welkome&about view$route with variable putting

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $name = "Alex Yakovlev";
+
+    return view('welkome', compact('name'));
+});
+
+Route::get('/about', function () {
+    return view('about');
 });
