27dc0e8d3d72fdb72e95d8ca4c2f54bd4c9b64d0
leffo <leffalt@gmail.com>
2020-12-18 11:18:40 +0700

    eloquent in tinker see scopeMethods learned

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    public function scopeIncomplete($query)
+    {
+        return $query->where('completed', 0);
+    }
 }
